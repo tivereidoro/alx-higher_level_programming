67bf3ae840be4cfa93a2163f3eb92a7f1d2649b3
tivereidoro <tivereidoro@yahoo.com>
2023-11-14 18:16:07 +0000

    Fix semistandard -bug

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 'use strict';
-let a = process.argv[2];
-let b = process.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 function add (a, b) {
   if (isNaN(a) || isNaN(b)) {
     return (NaN);
