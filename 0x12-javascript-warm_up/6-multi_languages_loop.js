7babdf93fca0544573609a789600ed44957eaf24
tivereidoro <tivereidoro@yahoo.com>
2023-11-13 17:38:46 -0800

    Fix semistandard bug

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-'use strict';
-let args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let arg in args) {
-  console.log(args[arg]);
+
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < args.length; i++) {
+  console.log(args[i]);
 }
