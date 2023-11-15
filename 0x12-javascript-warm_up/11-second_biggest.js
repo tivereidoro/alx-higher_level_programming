fe56a56449388a72039191650b1cade9424cdbdc
tivereidoro <tivereidoro@yahoo.com>
2023-11-15 13:15:51 +0000

    Test run function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-'use strict';
 
-function secondMax (arg) {
-  if (arg.length < 1 || arg.length === 1) {
-    return 0;
-  }
-  arg.sort();
-  return arg[arg.length - 2];
+if (process.argv.length > 3) {
+  const array = process.argv.slice(2).map(Number);
+
+  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
+  console.log(Math.max.apply(null, array));
+} else {
+  console.log(0);
 }
-
-const ARGUMENTS = process.argv.slice(2);
-console.log(secondMax(ARGUMENTS));
