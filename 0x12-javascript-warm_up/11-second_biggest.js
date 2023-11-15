1d1c7d0ed02d7abb8c7414dca4bd1077774e0fdb
tivereidoro <tivereidoro@yahoo.com>
2023-11-15 12:44:29 +0000

    Test run function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 'use strict';
-let nextMax = 0;
-let args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  nextMax = args[args.length - 2];
+
+function secondMax (arg) {
+  if (arg.length <= 1) {
+    return 0;
+  }
+  arg.sort();
+  return arg[arg.length - 2];
 }
-console.log(nextMax);
+
+const ARGUMENTS = process.argv.slice(2);
+console.log(secondMax(ARGUMENTS));
